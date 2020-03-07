9458256dc4de211d9a19df2a780493f5e41fb440
VimalanBK <41508509+VimalanBK@users.noreply.github.com>
2020-03-07 13:32:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Signup from './components/signup.js';
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <Signup/>
+                </div>
+        )
+    }
+}
+export default App;
